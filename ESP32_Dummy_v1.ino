8023426cd38cc6541f0b459753a850beb6f8d7ea
kishenkumar92 <kishenkumar92@gmail.com>
2026-04-11 11:28:48 +1200

    Update ESP32_Dummy_v1.ino

--- ESP32_Dummy_v1.ino
+++ ESP32_Dummy_v1.ino
@@ -46,8 +46,10 @@
 // =====================================================
 
 // WiFi — CHANGE THESE to your network
-const char* WIFI_SSID     = "MUGETSU_EXT";
-const char* WIFI_PASSWORD = "d8stracts";
+const char* WIFI_SSID     = "Kishen";
+const char* WIFI_PASSWORD = "12345678";
+//const char* WIFI_SSID     = "MUGETSU";
+//const char* WIFI_PASSWORD = "d8stracts";
 
 // Client / site identity
 const char* CLIENT_NAME  = "Paradise Resort";
